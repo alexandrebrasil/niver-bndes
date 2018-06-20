621e782ad487edbe947adf7631da87fab3662411
Alexandre Brasil <alexandre.brasil@gmail.com>
2018-06-19 23:47:02 -0300

    Bootstrap webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
     mode: 'production',
     output: {
-        path: __dirname + '/dist'
+        path: path.resolve(__dirname, './dist')
     },
     entry: './app/index.js',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+        }]
+    },
     plugins: [
-
+        new HtmlWebpackPlugin({
+            template: './app/index.html'
+        })
     ]
 }
