b3a5a7a02558420ee2d1a27966ff153eddaceb47
Alexandre Brasil <alexandre.brasil@gmail.com>
2018-06-20 02:07:44 -0300

    Versão não-responsiva, sem imagens

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import './estilos.scss';
+
+$(() => {
+    $(window).on('resize', () => alturaMosaico());
+    alturaMosaico();
+    
+    $('#popover .close a').on('click', () => {
+        console.log('close');
+        $('#popover').addClass('hidden')
+    });
+})
+
+function alturaMosaico() {
+    let mosaico = $('.mosaico'),
+        altura = mosaico.height(),
+        linhas = mosaico.find('.linha');
+    
+    linhas.eq(0).css('height', `${altura * 0.57}px`);
+    linhas.eq(1).css('height', `${altura * 0.43}px`);
+}
