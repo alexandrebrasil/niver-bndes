8143fc41e66bd5f3662591c4e7bcb57dae56ef69
Alexandre Brasil <alexandre.brasil@bndes.gov.br>
2018-06-20 14:13:30 -0300

    Ajustes de leiaute

--- app/index.js
+++ app/index.js
@@ -1,21 +1,30 @@
 import './estilos.scss';
 
-$(() => {
-    $(window).on('resize', () => alturaMosaico());
+$(function() {
+    $(window).on('resize', alturaMosaico);
     alturaMosaico();
     
-    $('#popover .close a').on('click', () => {
-        console.log('close');
-        $('#popover').addClass('hidden')
+    $('body').css('overflow', 'hidden');
+    $('#popover .close a').on('click', fecharPopover);
+    $('body').on('keydown', function(e) {
+        if(e.keyCode === 27) {
+            fecharPopover();
+        }
     });
 })
+
+function fecharPopover() {
+    $('#popover').addClass('hidden')
+    $('body').css('overflow', 'auto');
+}
 
 function alturaMosaico() {
     let mosaico = $('.mosaico'),
         altura = mosaico.height(),
-        linhas = mosaico.find('.linha');
+        linhas = mosaico.find('.linha'),
+        largura = $(document).width();
     
-    if($(document).width() > 1210) {
+    if(largura > 1210) {
         linhas.eq(0).css('height', `${altura * 0.57}px`);
         linhas.eq(1).css('height', `${altura * 0.43}px`);
     } else {
